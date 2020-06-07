4cd1ae0ebaba4a2ba4647602f4537e76c6e60089
midnightgammr <58647666+midnightgammr@users.noreply.github.com>
2020-06-07 13:55:29 -0500

    call for code team name change

--- covid-test-connection.py
+++ covid-test-connection.py
@@ -4,32 +4,29 @@ from cloudant.result import Result
 
 """
     Team: Backend
-    MindsOfTech 
+    Call for code Team: MindsOfTech 
 """
 
 
-## IBM Cloudant credentials. 
+# IBM Cloudant credentials.
 pyusr = "7e89eed1-ada0-47e1-b1d0-2e72dbcf1c45-bluemix"
 pypsswrd = "da7e4ceffecb5a1c35e569bb68652987bb443c0402cc4e6d3f911f1d7335b772"
 pyapikey = "4fkmluaOyQ2khPzOTwtZnIZDNCsNEtRbfqae_wuZsWVA"
 
-
-
-## Connection to IBM Cloudant 
+# Connection to IBM Cloudant
 client = Cloudant.iam(pyusr, pyapikey, connect=True)
 client.connect()
 
 
-## Test connection to Cloudent db.
+# Test connection to Cloudent db.
 def connectionTest(db):
-  try:
-    db_name = db
-    told = client[db_name]
-  except:
-    print (f" '{db_name}' NOT FOUND ")
-  else:
-    print(f" '{db_name}' FOUND")
+    try:
+        db_name = db
+        told = client[db_name]
+    except:
+        print(f" '{db_name}' NOT FOUND ")
+    else:
+        print(f" '{db_name}' FOUND")
 
-  
 
 connectionTest('coviddev_db')
