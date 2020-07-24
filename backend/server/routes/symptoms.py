d8fdafc18aea1723d2d4d41b4c0d07fcf2268122
DAMIAN GRAHAM <58647666+midnightgammr@users.noreply.github.com>
2020-07-23 21:08:28 -0500

    Update symptoms.py
    variable name changes

--- backend/server/routes/symptoms.py
+++ backend/server/routes/symptoms.py
@@ -8,7 +8,7 @@ from server import app, cloud_db  # pull in Flask and database instance
 @app.route('/userSymptoms', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 def user():
     if request.method == 'GET':
-        usr = request.form.get('username')
+        usrnm = request.form.get('username')
         cnt = request.form.get('countries')
         symptomsList = request.form.getlist('symptoms')
         testList = request.form.getlist('tested')
@@ -17,11 +17,11 @@ def user():
         hshold = request.form.get('confirmedex')
 
         data = {
-            '_id': usr,
+            '_id': usrnm,
             'Country': cnt,
             'Symptoms': symptomsList,
             'Tested For': testList,
-            'Last Influenza Test Date': isdate,
+            'Last Influenza Test Date': lastDate,
             'Have you been exposed to someone confirmed with COVID19': confirmedex,
             'Does anyone in your household shows Symptoms of covid19': hshold,
             'type': '-Symptoms-'
