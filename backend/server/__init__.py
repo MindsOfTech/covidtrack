0ec6489da5d6cea813f0ceb28a210066b98675cc
DAMIAN GRAHAM <58647666+midnightgammr@users.noreply.github.com>
2020-09-18 01:16:14 -0500

    Update __init__.py

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -27,9 +27,7 @@ app.config['SWAGGER_UI_JSONEDITOR']=True
 
 <<<<<<< HEAD
 
-ACCOUNT_NAME = "7e89eed1-ada0-47e1-b1d0-2e72dbcf1c45-bluemix"
-API_KEY = "8GlK-4CkdvhsteVdNIGfvEQEifYk5YiyXZpAoVzIEd9w"
-app.config['SECRET_KEY'] = 'piday@@DDSD'
+
 =======
 ACCOUNT_NAME = ""
 API_KEY = ""
